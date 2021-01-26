bb02685df07d71e404719be3803c474f7e8a62f7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-01-27 02:02:32 +0530

    spinner - assignment - solution

--- JS-Patterns/spinner.js
+++ JS-Patterns/spinner.js
@@ -1,4 +1,19 @@
-var spinner = /* fill in the blanks */
+var spinner = (function(){
+    var counter = 0;
+    
+    function up(){
+        return ++counter;
+    }
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    };
+})();
+
 
 //up(), down()
 
