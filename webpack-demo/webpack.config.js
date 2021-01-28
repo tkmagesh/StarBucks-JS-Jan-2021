f91fed51ef68da850e570abede19900c7e02c632
Magesh Kuppan <tkmagesh77@gmail.com>
2021-01-29 03:36:50 +0530

    using the html-webpack-plugin to create the index.html for the build

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry : './src/index.js',
     output : {
-        filename : 'bundle.js',
+        filename : 'bundle.[contenthash].js',
         path : path.resolve(__dirname, './dist')
     },
     mode : 'production', // 'none' , 'development', 'production'
@@ -32,8 +33,23 @@ module.exports = {
     },
     plugins : [
         new MiniCssExtractPlugin({
-            filename : 'style.css'
+            filename : 'style.[contenthash].css'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(), 
+        new HtmlWebpackPlugin({
+            title : 'A new App',
+            templateContent : `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                </head>
+                <body>
+                    <div id="root"></div>
+                </body>
+                </html>
+            `
+        })
     ]
 }
